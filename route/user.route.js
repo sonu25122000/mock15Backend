faf022d66d1cc03fbb45d8c8422556e5b9d3f489
sonu25122000 <skg74827@gmail.com>
2023-04-11 12:05:16 +0530

    login and signup

--- route/user.route.js
+++ route/user.route.js
@@ -1,19 +1,57 @@
 const express = require("express");
 const userRouter = express.Router();
 const { userModel } = require("../model/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 userRouter.get("/", (req, res) => {
   res.send("USER ROUTE");
 });
 
-userRouter.post("/add", async (req, res) => {
-  const payload = req.body;
+userRouter.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.find({ email });
+
+  if (user.length <= 0) {
+    try {
+      bcrypt.hash(password, 5, async (err, hash) => {
+        if (err) {
+          res.send({ msg: "something went wrong", err: err.message });
+        } else {
+          const user = new userModel({
+            email,
+            password: hash,
+          });
+          await user.save();
+          res.send({ msg: "new user has been registered" });
+        }
+      });
+    } catch (error) {
+      res.send({ msg: "something went wrong", err: err.message });
+    }
+  } else {
+    res.send({ msg: "User Already exist, Please Login" });
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
   try {
-    let user = new userModel(payload);
-    await user.save();
-    res.send({ msg: "posted successfully" });
+    const user = await userModel.find({ email });
+    if (user.length > 0) {
+      bcrypt.compare(password, user[0].password, (err, result) => {
+        if (result) {
+          const token = jwt.sign({ UserId: user[0].id }, "sonu");
+          res.send({ msg: "Login Successful", token });
+        } else {
+          res.send({ msg: "Invalid credential" });
+        }
+      });
+    }else{
+      res.send({ msg: "User Not Found" });
+    }
   } catch (error) {
-    res.send({ msg: "Something went wrong ", error: error.message });
+    res.send({ msg: "Somethoing went wrong", error: error.message });
   }
 });
 
